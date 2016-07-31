83935049c3a3692fa046b326d4b614667cdd5764
surpmillet <surpmillet@126.com>
2016-07-31 19:03:53 +0800

    重构 测试框架 添加 gulp task - buildTest

--- test/src/testDepartment.js
+++ test/src/testDepartment.js
@@ -3,33 +3,26 @@
  */
 describe('部门接口', function () {
     var id;
+    var list;
+    var detail;
     it('获取部门列表 getLit', function () {
         return dept.getList()
             .then((data)=> {
+                list = data.department;
                 return data.errcode.should.equal(0);
             });
     });
 
     it('获取部门详情 getDetail', function () {
-        return dept.getList()
+        return dept.getDetail({id: list[1].id})
             .then((data)=> {
-                return {id: data.department[0].id};
-            })
-            .then(dept.getDetail.bind(dept))
-            .then((data)=> {
+                detail = data;
                 return data.errcode.should.equal(0);
             });
     });
 
     it('创建部门 create', function () {
-        var options = {
-            "name": '测试',
-        };
-        return dept.getList()
-            .then((data)=> {
-                options.parentid = data.department[0].id;
-                return options
-            }).then(dept.create.bind(dept))
+        return dept.create({parentid: list[0].id, 'name': '测试'})
             .then((data)=> {
                 id = data.id;
                 return data.errcode.should.equal(0);
@@ -37,11 +30,8 @@ describe('部门接口', function () {
     });
 
     it('更新部门 update', function () {
-        return dept.getDetail({id})
-            .then((data)=> {
-                return {id: id, name: '新' + data.name};
-            })
-            .then(dept.update.bind(dept))
+        detail.name = '新' + detail.name;
+        return dept.update(detail)
             .then((data)=> {
                 return data.errcode.should.equal(0);
             });
--- test/src/testUser.js
+++ test/src/testUser.js
@@ -2,97 +2,74 @@
  * Created by michao on 16/7/30.
  */
 describe('用户接口', function () {
+    var id;
+    var userlist;
+    var detail;
     var userid;
-    it('获取部门成员列表', function () {
+    before('获取部门ID', function () {
         return dept.getList()
             .then((data)=> {
-                return {department_id: data.department[1].id};
-            })
-            .then(user.getSimpleList.bind(user))
+                return id = data.department[1].id;
+            });
+    });
+    it('获取部门成员列表', function () {
+        return user.getSimpleList({department_id: id})
             .then((data)=> {
+                userlist = data.userlist;
                 return data.errcode.should.equal(0);
-            })
+            });
     });
 
     it('获取部门成员列表详情', function () {
-        return dept.getList()
-            .then((data)=> {
-                return {department_id: data.department[1].id};
-            })
-            .then(user.getList.bind(user))
+        return user.getList({department_id: id})
             .then((data)=> {
                 return data.errcode.should.equal(0);
-            })
+            });
     });
 
     it('获取成员详情', function () {
-        return dept.getList()
+        return user.getDetail({userid: userlist[0].userid})
             .then((data)=> {
-                return {'department_id': data.department[1].id};
-            })
-            .then(user.getSimpleList.bind(user))
-            .then((data)=> {
-                return {userid: data.userlist[0].userid};
-            })
-            .then(user.getDetail.bind(user))
-            .then((data)=> {
+                detail = data;
                 return data.errcode.should.equal(0);
-            })
+            });
     });
 
     it('创建成员', function () {
-        // this.skip();
         var options = {
             "name": "张三",
-            "mobile": "15913215421"
+            "mobile": "15913215421",
+            "department": [id]
         };
-        return dept.getList()
-            .then((data)=> {
-                options.department = [data.department[1].id];
-                return options;
-            })
-            .then(user.create.bind(user))
+        return user.create(options)
             .then((data)=> {
                 userid = data.userid;
                 return data.errcode.should.equal(0);
-            })
+            });
     });
 
     it('更新成员', function () {
-        // this.skip();
-        return user.getDetail({userid})
-            .then((data)=> {
-                return {
-                    userid: data.userid,
-                    name: data.name,
-                    position: '主任'
-                }
-            })
-            .then(user.update.bind(user))
+        detail.position = '测试';
+        return user.update(detail)
             .then((data)=> {
                 return data.errcode.should.equal(0);
-            })
+            });
     });
 
     it('删除成员', function () {
         return user.remove({userid})
             .then((data)=> {
                 return data.errcode.should.equal(0);
-            })
+            });
     });
 
     it('批量删除成员', function () {
-        // this.skip();
         var options = {
             "name": "张三",
-            "mobile": "15913215421"
+            "mobile": "15913215421",
+            department: [id]
         };
-        return dept.getList()
-            .then((data)=> {
-                options.department = [data.department[1].id];
-                return options;
-            })
-            .then(user.create.bind(user))
+        return user.create(options)
             .then((data)=> {
                 return {useridlist: [data.userid]};
             })
