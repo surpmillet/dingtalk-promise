f1fed67326bb29e7fc980ca93e5999b5343b7441
surpmillet <surpmillet@126.com>
2016-08-01 21:11:30 +0800

    添加 dingtalk/media接口的代码和测试

--- src/media.js
+++ src/media.js
@@ -3,16 +3,31 @@
  */
 import Base from './base';
 import fs from 'fs';
+import path from 'path';
+import Service from './service';
 class Media extends Base {
-  createMedia(filepath) {
+  getMediaData(filepath) {
     return new Promise((resolve, reject)=> {
       fs.readFile(filepath, function (err, data) {
         if (err) {
           return reject(err);
         }
-        return resolve(data);
+        return resolve({filepath, data});
       });
     });
+  }
+
+  buildFormData(options) {
+    var mime = {'.jpg': 'image', '.png': 'image'};
+    var filepath = options.filepath;
+    var data = options.data;
+    var boundary = Service.getNonceSecurityString();
+    var contentType = 'multipart/form-data; boundary=' + boundary;
+    var header = `--${boundary}\r\nContent-Disposition:form-data;name=\"media\";filename=\"${path.basename(filepath)}\"\r\nContent-Type:application/octet-stream\r\n\r\n`;
+    var headerBuffer = new Buffer(header, 'utf8');
+    var endBuffer = new Buffer(`\r\n--${boundary}--\r\n`, 'utf8');
+    var buffer = Buffer.concat([headerBuffer, data, endBuffer]);
+    return {query: {type: mime[path.extname(filepath)], 'media': header}, contentType, buffer};
   }
 }
 export default Media;
--- test/src/testMedia.js
+++ test/src/testMedia.js
@@ -1,23 +1,13 @@
 /**
  * Created by michao on 16/7/31.
  */
-var path = require('path');
 describe.only('多媒体接口', function () {
   var media_id;
   it('上传媒体', function () {
+    this.skip();
     var filepath = '/Users/michao/Downloads/1.jpg';
-    var type = 'image';
-    return media.createMedia(filepath)
-      .then((data)=> {
-        var boundary = Service.getNonceSecurityString();
-        var contentType = 'multipart/form-data; boundary=' + boundary;
-        var header = `--${boundary}\r\nContent-Disposition:form-data;name=\"media\";filename=\"${path.basename(filepath)}\"\r\nContent-Type:application/octet-stream\r\n\r\n`;
-        var headerBuffer = new Buffer(header, 'utf8');
-        // var beginBuffer = new Buffer(`--${boundary}\r\n`, 'utf8');
-        var endBuffer = new Buffer(`\r\n--${boundary}--\r\n`, 'utf8');
-        var buffer = Buffer.concat([headerBuffer, data, endBuffer]);
-        return {query: {type, 'media': header}, contentType, buffer};
-      })
+    return media.getMediaData(filepath)
+      .then(media.buildFormData.bind(media))
       .then(media.upload.bind(media))
       .then((data)=> {
         media_id = data.media_id;
